8b4215f1dbac4a74743f01492047a0a4be37a0bb
HarryGomar <106566755+HarryGomar@users.noreply.github.com>
2024-11-14 21:28:23 -0600

    Loading Screen Changes
    Loading Screen Changes

--- src/LoadingScreen.jsx
+++ src/LoadingScreen.jsx
@@ -1,8 +1,9 @@
-import { useProgress } from "@react-three/drei";
+import React from 'react';
+import { useProgress } from "@react-three/drei"; 
 
 export const LoadingScreen = ({ started, onStarted }) => {
   const { progress } = useProgress();
-    
+
   return (
     <div className={`loadingScreen ${started ? "loadingScreen--started" : ""}`}>
       <div className="loadingScreen__progress">
@@ -14,21 +15,37 @@ export const LoadingScreen = ({ started, onStarted }) => {
         />
       </div>
       <div className="loadingScreen__board">
-        <h1 className="loadingScreen__title">SPACE TRAIN V1.0</h1>
-        
-        <img src="./TrainPixel.png" className="imageTrain" alt="" />
-        <h4 className="loadingScreen__desc2"> Movement Controls Inside Train: </h4>
-        <h4 className="loadingScreen__desc2"> ↑ - W  | ↓ - S </h4>
-        <button
-          className="loadingScreen__button"
-          disabled={progress < 100}
-          onClick={onStarted}
-        >
-          Start
-        </button>
-        <h4 className="loadingScreen__desc2">(Currently no FULL mobile support)</h4>
+        <div className="loadingScreen__header">
+          <h1 className="loadingScreen__title">HARRY GOMAR</h1>
+        </div>
+        <div className="loadingScreen__content">
+          <div className="loadingScreen__feature">
+            <img src="./Interact.png" className="loadingScreen__feature__image" alt="Interact" />
+            <h2 className="loadingScreen__feature__title">INTERACT</h2>
+            <p className="loadingScreen__feature__description">Learn all about my projects</p>
+          </div>
 
-        
+          <div className="loadingScreen__feature">
+            <img src="./Walk.png" className="loadingScreen__feature__image" alt="Move" />
+            <h2 className="loadingScreen__feature__title">MOVE</h2>
+            <p className="loadingScreen__feature__description">Explore using [W] and [S]</p>
+          </div>
+
+          <div className="loadingScreen__feature">
+            <img src="./Find.png" className="loadingScreen__feature__image" alt="Find" />
+            <h2 className="loadingScreen__feature__title">FIND</h2>
+            <p className="loadingScreen__feature__description">Get to know me</p>
+          </div>
+        </div>
+        <div className="loadingScreen__footer">
+          <button
+            className="loadingScreen__button"
+            disabled={progress < 100}
+            onClick={onStarted}
+          >
+            START
+          </button>
+        </div>
       </div>
     </div>
   );
